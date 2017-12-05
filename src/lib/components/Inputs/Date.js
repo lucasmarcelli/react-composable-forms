38be6be628419314dace9bd311191aa2f6d067b1
Lucas Marcelli <lucasmarcelli33@gmail.com>
2017-12-05 12:12:59 -0500

    v0.7.6

---
+++ src/lib/components/Inputs/Date.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Date extends Component {
+    
+    constructor(props){
+        super(props);
+    }
+    
+    render(){
+        return(
+            <div></div>
+        )
+    }
+
+}
+
+export default Date;
